dd6888350f8cfb6d9c078f3daff3e0b62b3f20ed
Benjamin Walsh <bewh0001@stud.slu.se>
2025-03-25 15:31:51 +0100

    taxontools: updated

--- taxontools.py
+++ taxontools.py
@@ -2,6 +2,8 @@
 
 import click
 import taxdmp_tools
+from typing import TextIO
+import sys
 
 option_taxonomy = click.option(
    "--taxonomy",
@@ -18,11 +20,11 @@ def cli():
 @cli.command(help="Get ancestor taxon at a given taxonomic rank")
 @option_taxonomy
 @click.option(
-   "--taxid",
-   "taxid",
-   required=True,
-   type=click.INT,
-   help="Taxonomic ID to get ancestor from"
+   "--input",
+   "input_fp",
+   type=click.File('r'),
+   default=sys.stdin,
+   help="File with Taxonomic IDs to get ancestors from"
 )
 @click.option(
     "--name/--no-name",
@@ -41,17 +43,34 @@ def cli():
    default=False,
    help="Print ancestor taxon rank"
 )
-def taxid2ancestor(taxid: int, target_rank: str, name: bool, rank: bool, taxonomy: str):
+def taxid2ancestor(input_fp: str, target_rank: str, name: bool, rank: bool, taxonomy: str):
+    taxids = parse_taxids(input_fp)
     taxa = taxdmp_tools.create_taxa(taxonomy=taxonomy)
-    new_taxid = taxdmp_tools.get_ancestor_at_rank(
-        first_taxid=taxid, target_rank=target_rank, taxa=taxa
-    )
-    fields = [str(new_taxid)]
+
+    new_taxids = list(map(
+        lambda taxid: taxdmp_tools.get_ancestor_at_rank(
+            first_taxid=taxid, target_rank=target_rank, taxa=taxa
+        ), taxids
+    ))
+
+    for taxid in new_taxids:
+        print(format_taxon_output(taxid=taxid, name=name, rank=rank, taxa=taxa))
+
+def format_taxon_output(taxid: int, name: bool, rank: bool, taxa: dict):
+    fields = [str(taxid)]
     if name:
-        fields.append(taxa[new_taxid]["name"])
+        fields.append(taxa[taxid]["name"])
     if rank:
-        fields.append(taxa[new_taxid]["rank"])
-    print("\t".join(fields))
+        fields.append(taxa[taxid]["rank"])
+    return("\t".join(fields))
+
+def parse_taxids(input_fp: TextIO):
+    taxids = []
+    for line in input_fp:
+        if not line.strip():
+            continue
+        taxids.append(int(line.strip()))
+    return(taxids)
 
 if __name__ == "__main__":
     cli()
